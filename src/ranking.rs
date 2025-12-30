1ee3ddda2952ade070d509a84a4930a534928b00
reuzed <49994279+reuzed@users.noreply.github.com>
2025-12-30 11:56:58 +0000

    ranking

--- src/ranking.rs
+++ src/ranking.rs
@@ -1,6 +1,17 @@
 // Ranks some search results by a metric.
 
+use std::cmp::Ordering;
+
+pub fn rank_by(words: &Vec<String>, f: &dyn Fn(&String, &String) -> Ordering) -> Vec<String> {
+    let mut words = words.clone();
+    words.sort_by(f);
+    words
+}
 pub fn rank_by_length(words: &Vec<String>) -> Vec<String> {
+    rank_by(words, &|w1: &String, w2: &String| w1.len().cmp(&w2.len()))
+}
+
+pub fn rank_by_score(words: &Vec<String>) -> Vec<String> {
     let mut words = words.clone();
     words.sort_by(|w1, w2| w1.len().cmp(&w2.len()));
     words
---
+++ src/wordplay.rs
@@ -0,0 +1,7 @@
+// Find wordplay instances from a given dataset
+// Have a set off target words, use some large source of words to find wordplay.
+// Return structs with cerificates of the wordplay
+
+pub fn wordplay(targets: &Vec<String>, words: &Vec<String>) -> Vec<String> {
+
+}
