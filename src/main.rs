f6d43134b96ce21a5baf076f01abfb722643ca68
reuzed <49994279+reuzed@users.noreply.github.com>
2026-01-03 15:58:08 +0000

    anagrams

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use cryptic_finder::{
     data::{
         load_word_frequencies, read_counties, read_places, read_tube_stations,
         read_word_frequencies, read_words, save_word_frequencies,
-    },
-    utils::{lex_order, reverse},
+    }, examples::maximally_anagrammed_by_length, utils::{lex_order, reverse}
 };
 
 // Find the common cryptic wordplays from various data sources:
@@ -23,10 +22,12 @@ fn main() {
 
     // save_word_frequencies();
 
-    let tubes = read_tube_stations();
-    let places = read_places();
-    let counties = read_counties();
-    println!("{:?}", &tubes[..10]);
-    println!("{:?}", &places[..10]);
-    println!("{:?}", &counties[..10]);
+    // let tubes = read_tube_stations();
+    // let places = read_places();
+    // let counties = read_counties();
+    // println!("{:?}", &tubes[..10]);
+    // println!("{:?}", &places[..10]);
+    // println!("{:?}", &counties[..10]);
+
+    maximally_anagrammed_by_length();
 }
--- src/wordplay.rs
+++ src/wordplay.rs
@@ -2,6 +2,65 @@
 // Have a set off target words, use some large source of words to find wordplay.
 // Return structs with cerificates of the wordplay
 
-pub fn anagrams(targets: &Vec<String>, words: &Vec<String>) -> Vec<String> {
-    targets.iter().map(|s| s.to_string()).collect()
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug)]
+pub struct Anagram {
+    pub fodder: String,
+    pub targets: Vec<String>,
+    pub anagrams: Vec<String>,
+}
+impl Anagram {
+    pub fn canonical_target(&self) -> String {
+        let mut targets = self.targets.clone();
+        targets.sort();
+        targets[0].clone()
+    } 
+    pub fn unique_anagrams(&self) -> Vec<String> {
+        let mut unique_anagrams = Vec::new();
+        unique_anagrams.append(&mut self.targets.clone());
+        unique_anagrams.append(&mut self.anagrams.clone());
+        let unique_anagrams: HashSet<_> = unique_anagrams.into_iter().collect();
+        unique_anagrams.into_iter().collect()
+    }
+}
+
+fn sort_word(word: &str) -> String {
+    let mut chars: Vec<char> = word.to_ascii_lowercase().chars().collect();
+    chars.sort();
+    chars.iter().collect()
+}
+
+fn sorted_anagram_map(words: &Vec<String>) -> HashMap<String, Vec<String>>{
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+    for word in words.iter(){
+        let sorted_word = sort_word(word);
+        let fodder_anagrams = map.get_mut(&sorted_word);
+        match fodder_anagrams {
+            Some(fodder_anagrams_row) => fodder_anagrams_row.push(word.to_string()),
+            None => {map.insert(sorted_word, vec![word.to_string()]);}, 
+        };
+    }
+    map
+}
+
+pub fn find_anagrams(targets: &Vec<String>, words: &Vec<String>) -> Vec<Anagram> {
+    // Use sorted(w) == sorted(v) \implies anagrams(v,w)
+    // Form maps from fodder to anagrams on targets and words
+    // For each target, attach the corresponding anagrams in a struct and return
+    let targets_map = sorted_anagram_map(&targets);
+    let words_map = sorted_anagram_map(&words);
+    let mut anagrams = Vec::new();
+    for (fodder, targets) in targets_map.iter(){
+        match words_map.get(fodder) {
+            None => (),
+            Some(anas) => {
+                 anagrams.push(
+                Anagram { fodder: fodder.to_string(), targets: targets.clone(), anagrams: anas.to_vec() }
+            )
+            }
+        }
+    }
+    // Ensure the word is more than just self-anagram
+    anagrams.into_iter().filter(|a| a.anagrams.len() > 1).collect()
 }
